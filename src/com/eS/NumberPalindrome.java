be3b0a097d47c907dffabf0e4a29c7dd8754dc10
es <StaNC2000>
2020-01-24 10:25:09 +0100

    Method created and tested

--- src/com/eS/NumberPalindrome.java
+++ src/com/eS/NumberPalindrome.java
@@ -3,10 +3,27 @@ package com.eS;
 public class NumberPalindrome {
 
     public static boolean isPalindrome(int number){
+        int reverse = 0;
+        int ifPalindrome = number;
+        int reminder;
+
+        while (ifPalindrome != 0){
+            reminder = ifPalindrome % 10;
+            reverse = reverse * 10 + reminder;
+            ifPalindrome = ifPalindrome / 10;
+
+            if (reverse == number){
+                return true;
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        System.out.println(isPalindrome(101));
+        System.out.println(isPalindrome(-100000001));
+        System.out.println(isPalindrome(10231));
     }
 }
