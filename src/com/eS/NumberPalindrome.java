f01edbe3516b470235f7fb9f08dd5252455a5338
es <StaNC2000>
2020-01-24 10:21:38 +0100

    Initial commit

---
+++ src/com/eS/NumberPalindrome.java
@@ -0,0 +1,12 @@
+package com.eS;
+
+public class NumberPalindrome {
+
+    public static boolean isPalindrome(int number){
+        return false;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
